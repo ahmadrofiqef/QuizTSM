e2ad1f8d1aadd0b9601116326c96a5d30d5cc37c
ahmadrofiqef <ahmadrofiqef@gmail.com>
2021-03-02 13:24:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.get('/', (req, res) => {
     var dateserver = new Date();
 //    res.send('ENV_NAME : '${process.env.NODE_ENV} 'DATE TIME : '${dateserver});
-    res.send(`ENV_NAME : ${process.env.NODE_ENV}, Add New line SERVER_TIME : ${dateserver.toISOString()}`);
+    res.send(`ENV_NAME : ${process.env.NODE_ENV}, Add New line for SERVER_TIME : ${dateserver.toISOString()}`);
 });
 
 // adding new lines of code
